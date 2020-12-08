c55fe11722564f5771e84812b916058410233ebf
mduduziavatar <mduduziavatar@gmail.com>
2020-12-08 02:09:23 +0200

    getting changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const exphbs = require("express-handlebars");
-const bodyParser = require("body-parser"); 	// add this line
+const bodyParser = require("body-parser"); // add this line
 const app = express();
 const pg = require("pg");
 const Pool = pg.Pool;
@@ -14,64 +14,70 @@ app.use(bodyParser.json()); // add  this line
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
-const connectionString = process.env.DATABASE_URL || 'postgresql://localhost:5432/choco';
+const connectionString = process.env.DATABASE_URL || 'postgresql://mdu:pg123@localhost:5432/choco';
 
 const pool = new Pool({
     connectionString
-  });
+});
 
 const chocolate = Chocolate(pool);
 
 
 // after you added  this  restart the app
-app.get("/", async function (req, res) {
+app.get("/", async function(req, res) {
 
-	const chocolates = await chocolate.list();
+    const chocolates = await chocolate.list();
 
-	res.render("index", { 
-		chocolates  
-	});
+    res.render("index", {
+        chocolates
+    });
 
 });
 
-app.get("/add", async function (req, res) {
+app.get("/add", async function(req, res) {
 
-	// const result = await pool.query("select * from chocolate");
+    const result = await pool.query("select * from chocolate");
 
-	res.render("add", { 
-		// chocolates : result.rows  
-	});
-	
+    res.render("add", {
+        chocolates: result.rows
+    });
+
 });
 
-app.post("/add", async function (req, res) {
+app.post("/add", async function(req, res) {
+    let chocName = req.body.name;
+    let qty = req.body.qty
 
-	// const result = await pool.query("select * from chocolate");
+    if (chocName && qty !== "") {
 
-	res.render("add", { 
-		// chocolates : result.rows  
-	});
-	
+        await chocolate.insert(chocName, qty)
+    }
+
+    const result = await chocolate.list()
+
+    res.render("index", {
+        chocolates: result
+    });
+
 });
 
-app.post("/chocolate_stock", async function(req, res){
-	let {
-		id
-	} = req.body;
+app.post("/chocolate_stock", async function(req, res) {
+    let {
+        id
+    } = req.body;
 
-	if (req.body["plus"] == "") {
-		await chocolate.incrementQtyById(id);
-	} else if (req.body["minus"] == "") {
-		await chocolate.decrementQtyById(id);
-	}
+    if (req.body["plus"] == "") {
+        await chocolate.incrementQtyById(id);
+    } else if (req.body["minus"] == "") {
+        await chocolate.decrementQtyById(id);
+    }
 
-	res.redirect("/");
+    res.redirect("/");
 });
 
 var portNumber = process.env.PORT || 3000;
 
 //start everything up
-app.listen(portNumber, function () {
+app.listen(portNumber, function() {
     console.log('Chocolate example server listening on:', portNumber);
 });
-
